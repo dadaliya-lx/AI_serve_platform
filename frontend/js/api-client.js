02d830ddb259413afc0cd4b345660e1cbea891cb
ren-junjie-15 <2213499332@qq.com>
2026-03-21 19:41:22 +0800

    Update API URL to production backend

--- frontend/js/api-client.js
+++ frontend/js/api-client.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://city-public-service-platfom.2213499332.workers.dev/api";
 
 async function apiRequest(url, options = {}) {
   const defaultOptions = {
